55c076a8867abce00171ccca3e9390ed5d3a6435
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-06 16:59:05 -0700

    working start bind button

--- javascripts/jsracer.js
+++ javascripts/jsracer.js
@@ -29,10 +29,23 @@ GameController.prototype.run = function(view) {
 
 GameView.prototype.bindStartButton = function(board) {
     $('#start').on('click', function() {
+        board.resetBoard();
         board.showBoard();
+        board.hideStartButton();
     })
 }
 
 Board.prototype.showBoard = function() {
     $('.racer_table td').css("visibility", "visible");
+}
+
+Board.prototype.hideStartButton = function() {
+    $('#start').css('visibility', "hidden")
+}
+
+Board.prototype.resetBoard = function() {
+    $('.racer_table td').removeClass();
+    $('#player1_strip td:nth-child(1)').addClass('active');
+    $('#player2_strip td:nth-child(1)').addClass('active');
+
 }
